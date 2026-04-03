6b7ae988a4fa9356c6760daa6875236b6f1aca69
jh-tpp <jonathan@total-portfolio.org>
2026-04-03 11:44:04 +0200

    Refactor SystemVisualization to use buttons and hooks

--- components/SystemVisualization.tsx
+++
@@ -1,7 +0,0 @@
-export default function SystemVisualization() {
-  return (
-    <div className="rounded-2xl border border-dashed border-slate-300 p-8 text-slate-600">
-      System visualization migration placeholder
-    </div>
-  );
-}
